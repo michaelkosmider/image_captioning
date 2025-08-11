bd6205a3131df3ed0a149cd7e4fc6d877c9013d6
Michael Kosmider <michael.kosmider24@gmail.com>
2025-08-11 13:18:13 -0400

    minor update to transforms file

--- image_transforms.py
+++ image_transforms.py
@@ -4,7 +4,7 @@ __all__ = ["encoder_image_transform_index", "captioner_image_transform_index"]
 
 encoder_train_image_transform = transforms.Compose(
     [
-        transforms.RandomResizedCrop(224, scale=(0.5, 1.0), interpolation=3),
+        transforms.RandomResizedCrop(224, scale=(0.2, 1.0), interpolation=3),
         transforms.RandomHorizontalFlip(p=0.5),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
